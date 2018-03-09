16876e8889e58bcdf4d0f26ba26ffcf127f5b8d1
DaddyBullet <notna989898@gmail.com>
2018-03-09 17:52:59 +0200

    Init commit

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 9, 2018
+ *      Author: antonbogovis
+ */
+
+
+#include <stdio.h>
+#include <stdint.h>
+
+struct SimplexTable{
+	uint32_t rows;
+	uint32_t cols;
+
+
+};
+
+int main(int argn, char *args[])
+{
+	printf("hi");
+	return 0;
+}
