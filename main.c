8b58dc01f8eaa067a8b187d2187196d99d6f984c
DaddyBullet <notna989898@gmail.com>
2018-03-16 00:47:01 +0200

    Input parametrs changed

--- main.c
+++ main.c
@@ -21,26 +21,38 @@ int main(int argn, char *args[])
         printf("Task doesn't defined\n");
         return 0;
     }
-    if(argn < 3)
-    {
-    	printf("No destination file defined\n");
-		return 0;
-    }
 
     FILE *simplex_file = fopen(args[1], "r");
+    FILE *output = stdout;
     if(simplex_file == NULL)
     {
     	printf("Can't read from %s!", args[1]);
     	return 0;
     }
-    FILE *output = fopen(args[2], "w");
-    if(output == NULL)
-	{
-		printf("Can't write to %s!", args[2]);
-		return 0;
-	}
+    if(argn > 2)
+    {
+    	if(strlen(args[2])<=2)
+    		delim = args[2][0];
+    	else
+    	{
+			output = fopen(args[2], "w");
+			if(output == NULL)
+			{
+				printf("Can't write to %s!", args[2]);
+				return 0;
+			}
+    	}
+    }
+
     if(argn > 3)
-    	delim = args[3][0];
+	{
+    	output = fopen(args[3], "w");
+		if(output == NULL)
+		{
+			printf("Can't write to %s!", args[2]);
+			return 0;
+		}
+	}
 
     int failed = parseSimplexFile(&st, simplex_file, delim);
     fclose(simplex_file);
